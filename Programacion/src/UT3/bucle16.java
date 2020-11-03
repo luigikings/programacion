61a1fd5a0ec19f588c149593e568fa732c83940a
LuisDaSilva <LuisDaSilva@DESKTOP-KF8T2TO.localdomain>
2020-11-03 13:44:26 +0100

    bucle de ejercicio 16 - 18

--- Programacion/src/UT3/bucle16.java
+++ Programacion/src/UT3/bucle16.java
@@ -18,26 +18,23 @@ public class bucle16 {
 		// TODO Auto-generated method stub
 		
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Escriba numero de Inicio");
-		int n1 = sc.nextInt();
-		System.out.println("Escriba numero de Final");
-		int n2 = sc.nextInt();
+		int n1;
+		int n2;
+		do { 
+			System.out.println("Escriba numero de Inicio");
+			n1 = sc.nextInt();
+			System.out.println("Escriba numero de Final");
+			n2 = sc.nextInt();
+		} 
+		while (n2 < n1);
 		System.out.println("Escriba numero de Intervalos");
 		int intervalo = sc.nextInt();
 		
-		if (n2<n1) {
-			int intercambio;
-			intercambio = n2;
-			n2 = n1;
-			n1 = intercambio;
-		} else;
+	
 		
 		for(int i = n1; i<=n2; i=i+intervalo){
 			System.out.print(i + " ");
 		}
-		
-		
 
 	}
 
