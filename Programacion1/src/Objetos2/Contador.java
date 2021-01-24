9f305c86d3e8dd765c1e518ec42e30f847e1935e
luigi <luigi@DESKTOP-7NKQHTP>
2021-01-24 15:40:46 +0100

    Objetos 2

---
+++ Programacion1/src/Objetos2/Contador.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package Objetos2;
+
+/**
+ * @author Luigi
+ *
+ */
+public class Contador {
+	
+	public int cont;
+	
+	public Contador() {}
+	
+	public Contador(int cont) {
+		setCont(cont);
+	}
+	
+	public Contador(Contador contador2) {
+		this.cont=contador2.cont;
+	}
+	
+	public int getCont() {
+		return cont;
+	}
+	
+	public void setCont(int cont) {
+		if(cont<0) {
+			this.cont=0;
+		}else {
+			this.cont=cont;
+		}
+	}
+	public void incrementar() {
+		cont++;
+	}
+	public void decrementar() {
+		cont--;
+		if(cont<0) {
+			cont=0;
+		}
+	}
+	
+	
+	
+}
