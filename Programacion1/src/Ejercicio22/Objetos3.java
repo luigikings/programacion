3663e2285f7a001b1173b3d73ec2aa7ad6d3b642
luigi <luigi@DESKTOP-7NKQHTP>
2021-01-28 23:40:08 +0100

    Objetos 3

---
+++ Programacion1/src/Ejercicio22/Objetos3.java
@@ -0,0 +1,33 @@
+package Ejercicio22;
+
+import java.util.Scanner;
+
+public class Objetos3 {
+
+	public static void main(String[] args) {
+		libro libro1=new libro("la casa de la pradera","chiquito de la calzada",10,9);
+		libro libro2=new libro("el ultimo mohicano","kiko rivera",5,4);
+		libro1.prestamo();
+		System.out.println("Ejemplares del libro 1: "+libro1.getNumEjemplares());
+		libro1.prestamo();
+		System.out.println("Ejemplares del libro 1: "+libro1.getNumEjemplares());
+		libro1.devolver();
+		libro1.setNumPrestados(0);
+		libro1.devolver();
+		System.out.println("Ejemplares prestados del  libro 1: "+libro1.getNumPrestados());
+		System.out.println("Vamos a Introducir tu propio libro ahora. USA LA IMAGINACION!!!");
+		libro libro3=new libro();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Dime el nombre de tu libro: ");
+		libro3.setTitulo(sc.nextLine());
+		System.out.println("Dime el Autor de tu libro: ");
+		libro3.setAutor(sc.nextLine());
+		System.out.println("Dime el numero de Libros que tienes: ");
+		libro3.setNumEjemplares(sc.nextInt());
+		System.out.println("Dime el numero de Ejemplares Prestados tienes: ");
+		libro3.setNumPrestados(sc.nextInt());
+		System.out.println(libro3.toString());
+		sc.close();
+	}
+
+}
---
+++ Programacion1/src/Ejercicio22/libro.java
@@ -0,0 +1,75 @@
+package Ejercicio22;
+ 	/**
+ 	 * 
+ 	 * @author Luigi
+ 	 *
+ 	 */
+public class libro {
+	
+	private String Titulo;
+	private String Autor;
+	private int numEjemplares;
+	private int numPrestados;
+	
+	/**
+	 * Constructor libro vacio
+	 */
+	public libro() {}
+	public libro(String titulo,String autor,int numeroEjemplares,int numPrestados) {
+
+		this.Titulo=titulo;
+		this.Autor=autor;
+		this.numEjemplares=numeroEjemplares;
+		this.numPrestados=numPrestados;
+	}
+	
+	public void prestamo() {
+		if (numEjemplares>numPrestados) {
+			numPrestados++;
+			System.out.println("El prestamo se ha efectuado con exito");
+		}else {
+			System.out.println("No se puede prestar el libro por falta de Ejemplares");
+		}
+	}
+
+	public void devolver() {
+
+		if (numPrestados==0) {
+			System.out.println("Este libro no se a prestado, No se ha hecho la devolucion del mismo");
+		}else {
+			numPrestados--;
+			System.out.println("Devolucion con exito.");
+		}
+	}
+	
+	public String toString() {
+		return "Titulo del Libro: "+getTitulo()+"\nNombre del autor: "+getAutor()+"\nEjemplares del libro: "+getNumEjemplares()+"\nLibros prestados: "+getNumPrestados();
+	}
+	
+	
+	public String getTitulo() {
+		return Titulo;
+	}
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+	public String getAutor() {
+		return Autor;
+	}
+	public void setAutor(String autor) {
+		Autor = autor;
+	}
+	public int getNumEjemplares() {
+		return numEjemplares;
+	}
+	public void setNumEjemplares(int numEjemplares) {
+		this.numEjemplares = numEjemplares;
+	}
+	public int getNumPrestados() {
+		return numPrestados;
+	}
+	public void setNumPrestados(int numPrestados) {
+		this.numPrestados = numPrestados;
+	}
+	
+}
